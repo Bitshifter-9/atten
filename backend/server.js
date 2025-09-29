002ab9985d1da85da7a4b7647be8269e6d9bf4f5
Bitshifter-9 <bandarampranav@gmail.com>
2025-09-29 19:48:31 +0530

    DVFsv

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,24 @@ require("dotenv").config();
 const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  "https://atten-icti.vercel.app",
+  "https://atten-icti-1ddvi1a6p-pranavs-projects-571017c0.vercel.app",
+];
 
-app.use(cors({ origin: "https://atten-icti.vercel.app" }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+app.use(cors());
+
 app.use(express.json());
 
 // Auth middleware
